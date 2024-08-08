3812791a687e7905f11b5c9ec5d363fb9acc86b0
CristyNel <nelly_4te@yahoo.it>
2024-08-08 14:42:01 +0100

    chore: Update users table schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,25 +1,25 @@
-DROP TABLE IF EXISTS user;
-CREATE TABLE user (
-  id                INT AUTO_INCREMENT  PRIMARY KEY,
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id                INT IDENTITY(1,1) PRIMARY KEY,
   jobtitle          VARCHAR(128) NOT NULL,
   firstname         VARCHAR(128) NOT NULL,
   lastname          VARCHAR(128) NOT NULL,
   email             VARCHAR(128) NOT NULL,
   phone             VARCHAR(128) NOT NULL,
-  address            VARCHAR(128) NOT NULL,
+  address           VARCHAR(128) NOT NULL,
   city              VARCHAR(128) NOT NULL,
   country           VARCHAR(128) NOT NULL,
   postalcode        VARCHAR(128) NOT NULL,
   dateofbirth       DATE NOT NULL,
   nationality       VARCHAR(128) NOT NULL,
-  summary           VARCHAR(256),
-  workexperience    VARCHAR(128),
-  education         VARCHAR(128),
-  skills            VARCHAR(128),
-  languages         VARCHAR(128)
+  summary           VARCHAR(256) NULL,
+  workexperience    VARCHAR(128) NULL,
+  education         VARCHAR(128) NULL,
+  skills            VARCHAR(128) NULL,
+  languages         VARCHAR(128) NULL
 );
 
 CREATE TABLE template (
-  id            INT AUTO_INCREMENT  PRIMARY KEY,
+  id            INT IDENTITY(1,1) PRIMARY KEY,
   path          VARCHAR(128) NOT NULL
 );
