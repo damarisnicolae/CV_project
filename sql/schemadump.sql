f2f5512497c5616ecf3e73fa017822dd4fe4a42c
CristyNel <nelly_4te@yahoo.it>
2024-08-09 03:59:04 +0100

    Update users and template table

--- sql/schemadump.sql
+++ sql/schemadump.sql
@@ -1,6 +1,9 @@
+DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS template;
+
 CREATE TABLE users (
-  id                INT IDENTITY(1,1) PRIMARY KEY,
+  id                INT AUTO_INCREMENT PRIMARY KEY,
   jobtitle          VARCHAR(128) NOT NULL,
   firstname         VARCHAR(128) NOT NULL,
   lastname          VARCHAR(128) NOT NULL,
@@ -12,15 +15,15 @@ CREATE TABLE users (
   postalcode        VARCHAR(128) NOT NULL,
   dateofbirth       DATE NOT NULL,
   nationality       VARCHAR(128) NOT NULL,
-  summary           VARCHAR(256) NULL,
-  workexperience    VARCHAR(128) NULL,
-  education         VARCHAR(128) NULL,
-  skills            VARCHAR(128) NULL,
-  languages         VARCHAR(128) NULL
+  summary           VARCHAR(256),
+  workexperience    VARCHAR(128),
+  education         VARCHAR(128),
+  skills            VARCHAR(128),
+  languages         VARCHAR(128)
 );
 
 CREATE TABLE template (
-  id            INT IDENTITY(1,1) PRIMARY KEY,
+  id            INT AUTO_INCREMENT PRIMARY KEY,
   path          VARCHAR(128) NOT NULL
 );
 
