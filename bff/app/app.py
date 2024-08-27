cb72602466f0c1b7640eafc190f2ac74281ec526
CristyNel <nelly_4te@yahoo.it>
2024-08-27 12:14:30 +0100

    Update bff structure and templates

--- bff/app/app.py
+++ bff/app/app.py
@@ -9,7 +9,7 @@ args = vars(parser.parse_args())
 IP = args["ip"]
 PORT = args["port"]
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates')
 
 @app.route('/', methods=['GET'])
 def home():
@@ -21,13 +21,13 @@ def home():
     except requests.exceptions.RequestException as e:
         app.logger.error(f"Request failed: {e}")
         abort(500, description="Internal Server Error")
-    return render_template('home.html', users=data)
+    return render_template('view/home.html', users=data)
 
 @app.route('/user', methods = ['POST'])
 def add_user():
     url = f"http://{IP}:{PORT}/user"
     requests.post(url = url)
-    return render_template('home.html')
+    return render_template('view/home.html')
 
 @app.route('/user/<id>', methods = ['PUT'])
 def edit_user(id):
@@ -35,18 +35,18 @@ def edit_user(id):
     url = f"http://{IP}:{PORT}/user/{id}"
     headers = {"Content-Type": "application/json"}
     requests.put(url, data=json.dumps(edited_data), headers=headers)
-    return render_template('home.html')
+    return render_template('view/home.html')
 
 @app.route('/postform', methods = ['GET'])
 def get_postform():
-    return render_template('post_form.html')
+    return render_template('forms/post_form.html')
 
 @app.route('/editform', methods = ['GET'])
 def get_user():
     url = f"http://{IP}:{PORT}/user"
     u = requests.get(url = url)
     data = u.json()
-    return render_template('edit_form.html', data = data)
+    return render_template('forms/edit_form.html', data = data)
 
 @app.route('/template1', methods = ['GET'])
 def generate_template1():
@@ -58,7 +58,7 @@ def generate_template1():
     
     r2 = requests.get(url = url2) ####todo
     
-    return render_template("template1.html", data = data)
+    return render_template("view/template1.html", data = data)
 
 @app.route('/template2', methods = ['GET'])
 def generate_template2():
@@ -68,7 +68,7 @@ def generate_template2():
     r = requests.get(url = url)
     data = r.json()
     
-    return render_template("template2.html", data = data)
+    return render_template("view/template2.html", data = data)
 
 @app.route('/template3', methods = ['GET'])
 def generate_template3():
@@ -78,32 +78,32 @@ def generate_template3():
     r = requests.get(url = url)
     data = r.json()
     
-    return render_template("template3.html", data = data)
+    return render_template("view/template3.html", data = data)
 
 @app.route('/login', methods = ['GET','POST'])
 def loginuser():
     if request.method == "GET":
-        return render_template('loginform.html')
+        return render_template('forms/loginform.html')
     if request.method == "POST":
         r = requests.post(f"http://{IP}:{PORT}/login", request.form, headers=request.headers)
         if r.status_code == 200:
             # data = r.json()
-            return render_template('greet.html')
+            return render_template('view/greet.html')
         else:
-            return render_template('loginform.html')
+            return render_template('forms/loginform.html')
     
 @app.route('/logout', methods = ['GET'])
 def logoutuser():
     r = requests.post(f"http://{IP}:{PORT}/logout")
-    return render_template('home.html')
+    return render_template('view/home.html')
 
 @app.route('/signup', methods = ['GET','POST'])
 def signupuser():
     if request.method == "GET":
-        return render_template('signupform.html')
+        return render_template('forms/signupform.html')
     if request.method == "POST":
         r = requests.post(f'http://{IP}:{PORT}/signup', request.form, headers=request.headers)
-        return render_template('home.html')
+        return render_template('view/home.html')
     
 if __name__=='__main__': 
     app.run(host='0.0.0.0', port=5000, debug=True)
